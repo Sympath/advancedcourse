5e6415d72bd7d4532ed4088bb48fc39f4aef7731
wzy <3101885298@qq.com>
2019-06-25 16:17:14 +0800

    原生实现antd弹窗组件

---
+++ lm/react/ant-toast-component/src/toast/Notification.jsx
@@ -0,0 +1,61 @@
+import React,{Component} from 'react';
+import ReactDOM from "react-dom";
+import Notice from "./Notice";
+class Notification extends Component{
+    state = {
+        notices : []
+    }
+    removeNotice = (key)=>{
+        const { notices } = this.state;
+        this.setState({
+            notices: notices.filter(item => item.key != key)
+        })
+    }
+    getNoticeKey=()=>{
+        return `notice-${Date.now()}`
+    }
+    addNotice=(notice)=>{
+        console.log(notice);
+        
+        const {notices} = this.state;
+        let {
+            type,
+            content,
+            duration,
+            onClose
+        } = notice;
+        notice.key = this.getNoticeKey();
+        notices.push(notice);
+        this.setState({
+            notices
+        })
+        setTimeout(() => {
+            this.removeNotice(notice.key)
+        }, notice.duration);
+    }
+    render(){
+        return(
+            <div>
+                {
+                    this.state.notices.map(item=>{
+                        return <Notice {...item} key={item.key}/>
+                    })
+                }
+            </div>
+        )
+    }
+}
+function createNotification(params) {
+    const div = document.createElement('div');
+    const ref = React.createRef();
+    document.body.appendChild(div);
+    // ref放置在组件上，可以拿到组件的实例
+    ReactDOM.render(<Notification ref={ref}/>,div);
+    return {
+        addNotice(notice){
+            return ref.current.addNotice(notice)
+        }
+    }
+}
+
+export default createNotification();
